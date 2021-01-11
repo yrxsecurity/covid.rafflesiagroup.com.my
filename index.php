8c968bb59036b124d3fd3961e6a55fa3edde4966
Y.rX Security ID <75939008+yrxsecurity@users.noreply.github.com>
2021-01-11 10:36:50 +0700

    Update and rename index to index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+<title>Hacked By Yrx Security</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|Just+Another+Hand" rel="stylesheet">
+<link rel="icon" type="image/png" href="https://i.ibb.co/D5L2f71/tolol.jpg">
+<meta content='MuH4mMad H4DyaN R4mADhAn' name='description'/>
+<style>
+*{
+    margin-top:2px;
+}
+
+body{
+    background-position: center;
+    background-color:#000000;
+    height:98%;
+    width:99%;
+    background-attachment: fixed;
+    background-size:100% 117%;
+    background-image:url();
+}
+.foter span{
+    text-align: center;
+    font-family: Orbitron;
+    font-size: 300%;
+    color: silver;
+    text-shadow: 0 0 3px silver, 0px 0px 5px darkblue;
+}
+
+#sec:hover{
+    color:#000000;
+    text-shadow: 0 0 3px silver, 0px 0px 5px darkblue;
+}
+</style></head>
+<!-- Cursor Exs .ani , Exs.jpg-->
+<!--Script Di bwah-->
+<body oncontextmenu="return false" onkeydown="return false" onmousedown="return false">
+<center>
+<br>
+<br>
+<img src="https://i.ibb.co/3TNyvvW/20201211-095009.png" height="300" width="300">
+<br>
+<b><font size="10" color="red" face="courier new"><span id="sec">H</span></font><font size="10" color="white" face="courier new"><span id="sec">A</span></font><font size="10" color="white" face="courier new"><span id="sec">C</span></font><font size="10" color="white" face="courier new"><span id="sec">K</span></font><font size="10" color="white" face="courier new"><span id="sec">E</span></font><font size="10" color="white" face="courier new"><span id="sec">D</span></font><font size="10" color="red" face="courier new"><span id="sec"> BY</span></font><font size="10" color="red" face="courier new"><span id="sec">                       YrX</span></font>
+<br><br>
+<!-- Tulisan Kedua--><div id="foter" class="foter">
+<font size="5" color="red" face="courier new">You</font><font size="5" color="red" face="courier new"> are</font><font size="5" color="red" face="courier new"> Revolusion</font><font size="5" color="red" face="courier new"> of</font><font size="5" color="red" face="courier new"> Xploiter</font>
+<!--Tengah-->
+<font color="white" size="4" face="Courier new">
+<br>
+<br>
+<b>Defaced By <br></b>
+<b><font size="3" color="red">MuH4mMad H4DyaN R4mADhAn</font></b>
+<br><br><br><br>
+</center
+</font></body></html>
